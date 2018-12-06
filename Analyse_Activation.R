cc88d9c526e7c9f7d8032a60e103c2608d5040e1
Katell Maro <kmaro@ensc.fr>
2018-12-06 15:51:52 +0100

    ACP + regression linéaire

--- Analyse_Activation.R
+++ Analyse_Activation.R
@@ -1,3 +1,57 @@
 # Analyse de l'activation du signal BOLD
 
-signal <- readRDS("activation.Rdata")
+data <- readRDS("activation.Rdata")
+
+#Analyse descriptive
+summary(data)
+data <- data[ , - c(1:2)]
+plot(data)
+
+
+if(!require(PCAmixdata)) {
+  install.packages(("PCAmixdata"))
+  library(PCAmixdata)
+}
+
+
+res_acp <- PCAmix(data, ndim=2, graph=FALSE) #contribution des variables aux axes principaux
+res_acp$sqload #carrÃ© du coef de corrÃ©lation entre les axes principaux et les variables
+plot(res_acp, choice="sqload") 
+
+plot(res_acp,axes=c(1,2),choice="cor")
+
+
+
+#
+res <- lm(signal$PROD_G_Frontal_Inf_Tri_1_L ~ signal$Age + signal$Volume_Cerebral + signal$Index_Lateralisation_Hemispherique + signal$PROD_G_Angular_2_L + signal$PROD_G_Occipital_Lat_1_L + signal$PROD_G_Rolandic_Oper_1_L + signal$PROD_S_Sup_Temporal_4_L + signal$PROD_G_Hippocampus_1_L + signal$PROD_G_Frontal_Inf_Tri_1_R + signal$PROD_G_Angular_2_R + signal$PROD_G_Occipital_Lat_1_R + signal$PROD_G_Rolandic_Oper_1_R + signal$PROD_S_Sup_Temporal_4_R + signal$PROD_G_Hippocampus_1_R)
+summary(res)
+
+res <- lm(signal$PROD_G_Frontal_Inf_Tri_1_L ~ 1, data=data)
+step(res, ~ signal$Age + signal$Volume_Cerebral + signal$Index_Lateralisation_Hemispherique + signal$PROD_G_Angular_2_L + signal$PROD_G_Occipital_Lat_1_L + signal$PROD_G_Rolandic_Oper_1_L + signal$PROD_S_Sup_Temporal_4_L + signal$PROD_G_Hippocampus_1_L + signal$PROD_G_Frontal_Inf_Tri_1_R + signal$PROD_G_Angular_2_R + signal$PROD_G_Occipital_Lat_1_R + signal$PROD_G_Rolandic_Oper_1_R + signal$PROD_S_Sup_Temporal_4_R + signal$PROD_G_Hippocampus_1_R, trace = TRUE)
+
+res2 <- lm(signal$PROD_G_Frontal_Inf_Tri_1_L ~ signal$PROD_G_Frontal_Inf_Tri_1_R + 
+             signal$PROD_S_Sup_Temporal_4_L + signal$PROD_G_Angular_2_L + 
+             signal$Index_Lateralisation_Hemispherique + signal$PROD_G_Occipital_Lat_1_R + 
+             signal$PROD_S_Sup_Temporal_4_R + signal$PROD_G_Hippocampus_1_L + 
+             signal$PROD_G_Hippocampus_1_R, data = data)
+summary(res2)
+
+
+res3 <- lm(signal$PROD_G_Frontal_Inf_Tri_1_L ~ signal$PROD_G_Frontal_Inf_Tri_1_R + 
+             signal$PROD_S_Sup_Temporal_4_L + signal$PROD_G_Angular_2_L + 
+             signal$Index_Lateralisation_Hemispherique + signal$PROD_G_Occipital_Lat_1_R + signal$PROD_G_Hippocampus_1_L + 
+             signal$PROD_G_Hippocampus_1_R, data = data)
+summary(res3)
+
+
+residus.stud <- rstudent(res2) #studentiser permet d'égaliser les écarts types
+plot(residus.stud, ylab="Résidus studentisés", ylim=c(-3.5,3.5))
+abline(h=c(-2,0,2), lty=c(2,1,2), col=c(2,1,2)) #on peut supprimer ou garde les valeurs en dehors des lignes rouges
+plot(res2$fitted, res2$residuals)
+shapiro.test(res2$residuals)
+
+
+
+#ANOVA
+data <- readRDS("activation.Rdata")
+data <- data[ , c(5:7, 10:12, 14, 16:17)] #12, 10, 7, 5, 14, 16, 17, 11
